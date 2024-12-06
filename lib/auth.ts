8d88989d49e46269dba29f724186d38fa7400e2c
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-12-06 11:01:03 +0100

    create get user function

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,26 +1,56 @@
-import 'server-only'
-import { cookies } from "next/headers"
+import "server-only";
+import { cookies } from "next/headers";
+import { client } from "./client";
+import { profileSchema } from "./schemas";
 
 const setAccessToken = async (accessToken: string) => {
-  const cookieStore = await cookies()
+  const cookieStore = await cookies();
 
-  cookieStore.set('acces-token', accessToken, { httpOnly: true})
-}
+  cookieStore.set("acces-token", accessToken, { httpOnly: true });
+};
 
 const deleteAccessToken = async () => {
-  const cookieStore = await cookies()
+  const cookieStore = await cookies();
 
-  cookieStore.delete('acces-token')
-}
+  cookieStore.delete("acces-token");
+};
 
 const getAccessToken = async () => {
-  const cookieStore = await cookies()
+  const cookieStore = await cookies();
 
-  return cookieStore.get('access-token')
-}
+  return cookieStore.get("access-token");
+};
+
+const getUser = async () => {
+  const accessToken = await getAccessToken();
+
+  if (!accessToken) {
+    return { error: "Access token missing" };
+  }
+
+  try {
+    const response = await client.get("/profile", {
+      headers: {
+        Authorization: `Bearer ${accessToken.value}`,
+      },
+    });
+
+    const { data, error } = profileSchema.safeParse(response.data);
+    if (error) {
+      return null;
+    }
+
+    return data
+
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
 
 export const auth = {
   setAccessToken,
   deleteAccessToken,
-  getAccessToken
-}
+  getAccessToken,
+  getUser,
+};
--- lib/schemas.ts
+++ lib/schemas.ts
@@ -1,5 +1,7 @@
 import { z } from "zod";
 
+// action schemas
+
 export const signUpSchema = z.object({
   username: z.string().min(3, "Minimum 3 characters").max(20, "Maximum 20 characters"),
   password: z.string().min(6, "Minimum 6 characters"),
@@ -23,3 +25,12 @@ export const commentSchema = z.object({
   content: z.string().min(1, "Comment must contain at least 1 character"),
   post_id: z.string(),
 })
+
+// data schemas
+
+export const profileSchema = z.object({
+  username: z.string(),
+  id: z.string()
+})
+
+export type ProfileData = z.infer<typeof profileSchema>
